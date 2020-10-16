7890757f75edb373397c513f9decbd63cc0afbfb
Fernando <fernando.felix@student.csulb.edu>
2020-10-16 10:28:16 -0700

    boiler plate

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import login from "./pages/login"
 import signup from "./pages/signup"
 import editAccount from './pages/editAccount'
-import Boiler from './Utilities/boilerPlate'
 import './Styles.css'
 
 
