5394a462b46862ea17959128731684b90248c54c
Brian Drennan <briansdrennan@gmail.com>
2020-10-24 14:20:45 -0700

    modular component

--- src/pages/stats.js
+++ src/pages/stats.js
@@ -1,8 +1,14 @@
-import { render } from '@testing-library/react'
 import React, { Component } from 'react'
 import { Doughnut } from 'react-chartjs-2'
+import axios from 'axios'
+
+
 const Chart = require('react-chartjs-2').Chart;
 
+/*
+ * By defualt, the Doughnut char does not support a number at the center.
+ * This code helps support this functionality.
+*/
 const originalDoughnutDraw = Chart.controllers.doughnut.prototype.draw;
 Chart.helpers.extend(Chart.controllers.doughnut.prototype, {
     draw() {
@@ -17,7 +23,7 @@ Chart.helpers.extend(Chart.controllers.doughnut.prototype, {
         ctx.font = `${fontSize}em sans-serif`;
         ctx.textBaseline = 'middle';
 
-        const text = chart.config.data.datasets[0].data[0]
+        const text = `${chart.config.data.datasets[0].data[0]}%`
         const textX = Math.round((width - ctx.measureText(text).width) / 2);
         const textY = height / 2;
 
@@ -25,32 +31,56 @@ Chart.helpers.extend(Chart.controllers.doughnut.prototype, {
     }
 });
 
-const data = {
-    datasets: [{
-        data: [80, 50],
-        backgroundColor: [
-            'rgba(255, 99, 132, 0.5)',
-            'rgba(54, 162, 235, 0.2)',
-        ],
-    }],
-    labels: [
-        'Completed',
-        'Working'
-    ]
-}
 
-class Stats extends Component {
+class ProgressChart extends Component {
 
     render() {
         return (
             <div>
                 <h1>Goal Stats</h1>
                 <Doughnut
-                    data={data}
+                    data={{
+                        datasets: [{
+                            data: [
+                                this.props.total_goals, 
+                                this.props.goals_completed
+                            ],
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.5)',
+                                'rgba(54, 162, 235, 0.2)',
+                            ],
+                        }],
+                        labels: [
+                            'Completed',
+                            'Working'
+                        ]
+                    }}
                     options={{
                         cutoutPercentage: 70,
                         rotation: 120
                     }}
+                />
+            </div>
+        )
+    }
+}
+
+class Stats extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            total_goals: 90,
+            goals_completed: 10
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <ProgressChart
+                    total_goals={this.state.total_goals}
+                    goals_completed={this.state.goals_completed}
                 />
             </div>
         )
