6fe3c9accef2e02a3550bb30505d58870edefc44
Brian Drennan <briansdrennan@gmail.com>
2020-11-02 12:52:54 -0800

    messing around with colors

--- src/Utilities/myBar.js
+++ src/Utilities/myBar.js
@@ -1,30 +1,37 @@
-import React from 'react'
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import Typography from '@material-ui/core/Typography';
-import { IconButton } from '@material-ui/core';
+import React from "react"
+import AppBar from "@material-ui/core/AppBar"
+import Toolbar from "@material-ui/core/Toolbar"
+import AccountCircleIcon from "@material-ui/icons/AccountCircle"
+import Typography from "@material-ui/core/Typography"
+import { IconButton } from "@material-ui/core"
+import withStyles from "@material-ui/core/styles/withStyles"
 
+const styles = (theme) => ({
+  app: {
+    backgroundColor: "#6495ED",
+  },
+})
 
-export default class MyBar extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {};
-    }
-    render() {
-        return (
-            <AppBar className='app' position="relative">
-                <Toolbar >
-                    <IconButton href='/editAccount' color='inherit'>
-                        <AccountCircleIcon className='icon' />
-                    </IconButton>
-                    {/*Typogragpy is imported form material-ui can have different types of text if you change the variant and color*/}
-                    <Typography variant="h6" color="inherit" noWrap>
-                        {this.props.page}
-                    </Typography>
-                </Toolbar >
-            </AppBar>
-        )
-    }
+class MyBar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  render() {
+    const { classes } = this.props
+    return (
+      <AppBar className={classes.app} position="relative">
+        <Toolbar>
+          <IconButton href="/editAccount" color="inherit">
+            <AccountCircleIcon className="icon" />
+          </IconButton>
+          {/*Typogragpy is imported form material-ui can have different types of text if you change the variant and color*/}
+          <Typography variant="h6" color="inherit" noWrap>
+            {this.props.page}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    )
+  }
 }
-
+export default withStyles(styles)(MyBar)
