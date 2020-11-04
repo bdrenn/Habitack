99c61de76ca3458e4af1d79dd9655c1ea2e3d1ee
Fernando <fernando.felix@student.csulb.edu>
2020-11-04 13:06:15 -0800

    removed auth

--- functions/index.js
+++ functions/index.js
@@ -8,7 +8,7 @@ const auth = require("./util/auth")
 const cors = require("cors")
 app.use(cors())
 
-const { getAllGoals, addGoal, deleteGoal, editGoal } = require("./APIs/goals")
+const { getAllGoals, addGoal, deleteGoal, editGoal, addGoalPic } = require("./APIs/goals")
 
 const {
   loginUser,
@@ -21,10 +21,13 @@ const {
 } = require("./APIs/users")
 
 // Goals
-app.get("/goals", getAllGoals)
-app.post("/goal", addGoal)
-app.delete("/goal/:goalId", deleteGoal)
-app.put("/goal/:goalId", editGoal)
+
+app.get("/goals", getAllGoals);
+app.post("/goal", auth, addGoal);
+app.delete("/goal/:goalId", deleteGoal);
+app.put("/goal/:goalId", editGoal);
+app.post("/addGoalPic",auth, addGoalPic);
+
 
 // Users
 app.post("/login", loginUser)
@@ -34,6 +37,7 @@ app.put("/updateUser", auth, updateName)
 app.put("/updateEmail", updateEmail)
 app.put("/updatePass", updatePass)
 app.put("/changeDisplay", changeDisplay)
+
 
 const main = express()
 main.use("/api", app)
