b26a1c6da5fb59b0f119872c4a5ff18cf3d545fc
SalCSULB <55815297+SalCSULB@users.noreply.github.com>
2020-11-11 22:36:55 -0800

    added call to API and formated for display
    Made API call and formated the 'MM/DD/YYY' into YYYY-MM-DDThh:mm

--- src/pages/myCalendar.js
+++ src/pages/myCalendar.js
@@ -14,9 +14,8 @@ class myCalendar extends Component {
         super(props)
 
         this.state = {
-            goals: [{ "goalsId": "GNeY2hXoU2p5YmBHopzn", "title": "runAPI", "start": "11/1/2020", "end": "11/30/2020", "completion": false },
-                { "goalsId": "fZgE5LfOtMfHDVLOYOe2", "title": "laundryAPI", "start": "11/20/2020", "end": "11/20/2020", "completion": false },
-                { "goalsId": "tg4wFqMYAMx2cl8cA8ji", "title": "studyAPI", "start": "10/14/2020", "end": "10/15/2020", "completion": true }]
+            goals: [],
+            goalsAPI: []
         }
 
     }
@@ -26,7 +25,7 @@ class myCalendar extends Component {
             .get("/goals")
             .then((response) => {
                 this.setState({
-                    goals: response.data,
+                    goalsAPI: response.data,
                 })
             })
             .catch((err) => {
@@ -34,41 +33,40 @@ class myCalendar extends Component {
             })
     }
 
-    schedulerGen() {
-            this.state.goals.map((goal) => (
-                <Typography>
-                    Description: {goal.title}
-                            Start: {goal.start}
-                </Typography>
-            ))
+    formatter() {
+        //this.state.goals = [        //pull goal data from back end and place here 
+        //    { startDate: '2020-11-18T09:45', endDate: '2020-11-18T11:00', title: 'Read' },
+        //     { startDate: '2020-11-18T12:00', endDate: '2020-11-18T13:30', title: 'Go to a gym' }];
+        this.state.goalsAPI.map((goal, index) => (
+            this.state.goals[index] = { startDate: this.formatDate(goal.start, true) , endDate: this.formatDate(goal.end, false), title: goal.title }
+        ))
+    };
 
-    }
+    formatDate(date, startFlag) {
+        var year = date.slice(6, 10);
+        var dtg = year + "-";
+        var month = date.slice(0, 2)
+        dtg = dtg + month + "-";
+        var day = date.slice(3, 5);
+        if (startFlag) {
+            console.log('was true');
+            dtg = dtg + day + "T09:00";
+        }
+        else
+            dtg = dtg + day + "T09:10";
+        return dtg;
+    };
 
     render() {
         const currentDate = '2020-11';
-        const schedulerData = [        //pull goal data from back end and place here 
-            { startDate: '2020-11-18T09:45', endDate: '2020-11-18T11:00', title: 'Read' },
-            { startDate: '2020-11-18T12:00', endDate: '2020-11-18T13:30', title: 'Go to a gym' }];
 
+        { this.formatter() }
 
         return (
             //Main div box which will contain all the entire page, needed because must have one parent div for everything
             <div className='calendar'>
                 {/*App bar on top of the page that displays little user icon and name of the page */}
                 <MyBar page="My Calendar" />
-
-                <Container maxWidth="md">
-                    <Typography component="h3" >
-                       Testing the goal format
-                    </Typography>
-                    {this.state.goals.map((goal) => (
-                            <Typography>
-                            Description: {goal.title}
-                            Start: {goal.start}
-                            </Typography>
-                        ))}
-                </Container>
-
 
                 {/* This container is the essentially a <div> but can define the max allowed width */}
                 <Container maxWidth='lg' >
@@ -78,7 +76,7 @@ class myCalendar extends Component {
                             Habitact
                         </Typography>
                         <Scheduler
-                            data={schedulerData}
+                            data={this.state.goals}
                         >
                             <ViewState
                                 currentDate={currentDate}
