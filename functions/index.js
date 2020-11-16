a1e94903eeab1d80d595df24052330bf6d455a61
Fernando <fernando.felix@student.csulb.edu>
2020-11-16 12:16:37 -0800

    email notification

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@
 const functions = require("firebase-functions")
 const express = require("express")
 const app = express()
+const nodemailer = require("nodemailer")
 const auth = require("./util/auth")
 
 const cors = require("cors")
@@ -38,6 +39,61 @@ app.put("/updateUser", auth, updateName)
 app.put("/updateEmail", updateEmail)
 app.put("/updatePass", updatePass)
 app.put("/changeDisplay", changeDisplay)
+
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  //port: 587,
+  //secure: false, // true for 465, false for other ports
+  auth: {
+    user: "habitack2020@gmail.com", // generated ethereal user
+    pass: "csulb2020" // generated ethereal password
+  },
+});
+
+exports.welcomeMessage = functions.firestore.document('/users/{user}').onCreate((snap,context) => {
+  const firstName = snap.data().firstName;
+  const lastName = snap.data().lastName;
+  const email = snap.data().email;
+  return mail(firstName, lastName, email)
+})
+
+
+//send email to users when first create a goal
+function mail(firstName, lastName, email) {  
+    // send mail with defined transport object
+    console.log(email)
+    return transporter.sendMail({
+      from: 'Habitack Team <habitack2020@gmail.com>', // sender address
+      to: email, // list of receivers
+      subject: "Welcome to Habitack!", // Subject line
+      html: `<h1>Hello ${firstName} ${lastName},</h1> <br /> 
+      <p>Welcome to your journey towards self improvement! Below is an introduction guide to help get you started</p><br />
+      <p>Habitack is a habit tracker designed to facilitate users with tracking goals that can change your life for the better. <br />
+         Our job is to calculate your progress and show you results. Your job is to attack daily challenges with the help of habitack. </p> <br />
+         <p>Pages: </p> <br />
+         <ol> 
+          <li>Goals Page</li>
+            <ul>
+              <li>List goals for the user</li>
+              <li>Add new goal</li>
+              <li>Delete goal</li>
+              <li>Add photo to goal</li>
+            </ul>
+          <li>Stats Page</li>
+            <ul>
+              <li>Display the stats for a current goal</li>
+            </ul>
+          <li>Account Page</li>
+            <ul>
+              <li>Change account information</li>
+            </ul>
+         </ol>`
+  })
+  .then(r => r)
+  .catch(e => e)
+}
+
+
 
 
 const main = express()
